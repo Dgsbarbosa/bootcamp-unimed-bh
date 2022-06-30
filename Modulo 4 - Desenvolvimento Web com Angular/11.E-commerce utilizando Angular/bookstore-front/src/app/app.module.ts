f47ab25b8d93727930e7718e94ef1737a99d40ed
Dgsbarbosa <guinho.cq@gmail.com>
2022-06-29 21:06:29 -0300

    aula

---
+++ Modulo 4 - Desenvolvimento Web com Angular/11.E-commerce utilizando Angular/bookstore-front/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { BookstoreAppComponent } from './components/bookstore-app/bookstore-app.component';
+import { ProductListComponent } from './components/bookstore-app/product-list/product-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { FiltersComponent } from './components/bookstore-app/filters/filters.component';
+import { ProductItemComponent } from './components/bookstore-app/product-list/product-item/product-item.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    BookstoreAppComponent,
+    ProductListComponent,
+    FooterComponent,
+    FiltersComponent,
+    ProductItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
