8306b36821c793a7e0af06521b12f14bd7fe2d0b
Dgsbarbosa <guinho.cq@gmail.com>
2022-07-10 17:45:50 -0300

    aula

---
+++ Modulo 6 - Dominando o Ecossistem .NET com C#/3. Primeiros passos com .NET/myApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+            for (int i = 1; i <= numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Este é a passagem {i} do nosso for");   
+            }
+            
+        }
+    }
+}
