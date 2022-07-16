cb1d92646f65324154a72faa4a7a20be6e873a25
Dgsbarbosa <guinho.cq@gmail.com>
2022-07-15 21:51:04 -0300

    aula

---
+++ Modulo 6 - Dominando o Ecossistem .NET com C#/6. Trabalhando com Arquivos e Streams em C#/File_And_FileInfo/Program.cs
@@ -0,0 +1,56 @@
+﻿// importando o System Console, usado para que não seja necessario escrever console
+using static System.Console;
+
+//importando System.IO
+using System.IO;
+
+/*
+//Criando arquivo
+
+var path = Path.Combine(Environment.CurrentDirectory, "teste.txt");
+File.Create(path);
+*/
+
+
+//Criando arquivo com texto onde o usuario escolhe o nome do arquivo
+
+WriteLine("Digite o nome do arquivo");
+
+var nomeArquivo = ReadLine();
+
+WriteLine("Digite a extenção desejada (sem ponto)! EX.: doc, txt, html, ...");
+var extensao = ReadLine();
+
+//tratando caracteres invalidos e trocando por '-'
+foreach (var caracter in Path.GetInvalidFileNameChars())
+{
+    nomeArquivo = nomeArquivo.Replace(caracter, '-');
+}
+
+
+var path2 = Path.Combine(Environment.CurrentDirectory, $"{nomeArquivo}.{extensao}");
+
+CriarArquivo(path2);
+
+
+static void CriarArquivo(string path2)
+{
+    try
+    {
+        using var sw = File.CreateText(path2);
+        sw.WriteLine("Esta é a linha 1 do nosso File.CreateText");
+        sw.WriteLine("Esta é a linha 2 do nosso File.CreateText");
+        sw.WriteLine("Esta é a linha 3 do nosso File.CreateText");
+    }
+    catch
+    {
+
+        WriteLine("O nome do arquivo é inválido");
+        WriteLine("Evite caracteres especiais");
+    }
+
+    WriteLine("Digite enter para finalizar");
+
+}
+
+
